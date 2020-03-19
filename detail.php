42a9521cd4b04d5d982bc9322517a0e9e55fd495
olinafauziah <olinafzh1712@gmail.com>
2020-03-19 15:43:09 +0700

    revisidetail

--- detail.php
+++ detail.php
@@ -6,6 +6,7 @@ include 'koneksi.php';
 $query = $koneksi->query("SELECT * FROM `tb_pelanggan` WHERE id_pelanggan = '$id_detail'");
 $data = $query->fetch_assoc();
 ?>
+<!-- ling bootsrap -->
 
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
